047ccabc8b35d96c07a87d322b1df010bdb81451
Gabriel Jose <gabriel.josemelo@hotmail.com>
2023-03-20 09:13:42 -0300

    feat: create to do list form submit

--- dev-app/src/pages/to-do-list.ts
+++ dev-app/src/pages/to-do-list.ts
@@ -1,4 +1,5 @@
-import { html } from '../html/html.js'
+import { html, render } from '../html/html.js'
+import { ref } from '../html/ref.js'
 import { signal } from '../html/signal.js'
 
 type ToDoItem = {
@@ -8,19 +9,53 @@ type ToDoItem = {
 }
 
 export function toDoList() {
-  const list = signal<ToDoItem[]>([])
+  const ulRef = ref<HTMLUListElement>()
+
+  const rawToDoList = localStorage.getItem('lithen-router@to-do-list')
+  const toDoList = JSON.parse(rawToDoList ?? '[]')
+
+  const list = signal<ToDoItem[]>(toDoList)
+
+  function submitForm(event: Event) {
+    event.preventDefault()
+    const form = event.target as HTMLFormElement
+
+    const message = String(form.message.value)
+
+    if (!message) return
+
+    list.set(value => [...value, {
+      id: crypto.randomUUID(),
+      message,
+      checked: false
+    }])
+
+    render(html`
+      <div>
+        <span>${message}</span>
+        <input type="checkbox" checked />
+      </div>
+    `, ulRef.el!)
+  }
   
   return html`
     <h1>To Do List</h1>
-    <ul>
+    <ul ref=${ulRef}>
       ${list.get().map(item => {
         return html`
           <div>
             <span>${item.message}</span>
-            <input ${item.checked && 'checked'} />
+            <input
+              type="checkbox"
+              ${item.checked && 'checked'}
+            />
           </div>
         `
       })}
     </ul>
+    <form on-submit=${submitForm}>
+      <input name="message" placeholder="What to do..." />
+      <button>Add</button>
+    </form>
   `
 }
